6174c75c8a8dec5c3ae0a3fcbe148815548edc1f
Luofei <luofei@LuofeideMac.local>
2017-06-06 17:17:21 +0800

    pod 'PermissionScope' 添加权限框架

--- MY/Pods/Target Support Files/Pods-MY/Pods-MY-frameworks.sh
+++ MY/Pods/Target Support Files/Pods-MY/Pods-MY-frameworks.sh
@@ -90,9 +90,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/BCColor/BCColor.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PermissionScope/PermissionScope.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/BCColor/BCColor.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PermissionScope/PermissionScope.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
