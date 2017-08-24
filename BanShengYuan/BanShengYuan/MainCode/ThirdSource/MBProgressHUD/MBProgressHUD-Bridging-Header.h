9dc72ca03ef8d1b8b050368c5c6e9151673dae86
Luofei <luofei@LuofeideMac.local>
2017-08-24 16:44:22 +0800

    no message

--- BanShengYuan/BanShengYuan/MainCode/ThirdSource/MBProgressHUD/MBProgressHUD-Bridging-Header.h
+++ BanShengYuan/BanShengYuan/MainCode/ThirdSource/MBProgressHUD/MBProgressHUD-Bridging-Header.h
@@ -2,15 +2,20 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+
+#import "MJRefresh/MJRefresh.h"
+
+//fmPay
+//#import <FmOnlinePayApi/FmOnlinePayApi.h>
+//alipay
 #import <UIKit/UIKit.h>
-
 #import <AlipaySDK/AlipaySDK.h>
-#import "MJRefresh/MJRefresh.h"	
-
 //wechat
 #import "WXApi.h"
 #import "WXApiObject.h"
 
+#import <PgySDK/PgyManager.h>
+
 #import <Qiniu/QiniuSDK.h>
 
-#import <PgySDK/PgyManager.h>
+
