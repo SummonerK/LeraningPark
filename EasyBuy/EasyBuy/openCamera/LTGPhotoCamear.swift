438d04bda3e3630da2f2f71a73ce2402dee3eb8a
luofei <fei.luo@freemud.cn>
2018-03-15 18:21:55 +0800

    no message

--- EasyBuy/EasyBuy/openCamera/LTGPhotoCamear.swift
+++ EasyBuy/EasyBuy/openCamera/LTGPhotoCamear.swift
@@ -10,6 +10,8 @@ import UIKit
 import AVFoundation
 import Photos
 
+let currentResolutionW = CGFloat(720)
+let currentResolutionH = CGFloat(1280)
 
 @available(iOS 10.0, *)
 class LTGPhotoCamear: UIViewController {
@@ -23,8 +25,14 @@ class LTGPhotoCamear: UIViewController {
     private var session: AVCaptureSession?
     private var previewLayer: AVCaptureVideoPreviewLayer?
     
+    var cropX:Int! = nil
+    var cropY:Int! = nil
+    
 //    fileprivate var showImageContainerView: UIView?
     @IBOutlet weak var showImageView: UIImageView!
+    
+    @IBOutlet weak var label_msg: UILabel!
+    
     fileprivate var picData: Data? = nil{
         didSet{
             
@@ -46,6 +54,8 @@ class LTGPhotoCamear: UIViewController {
         
         TGPhotoPickerManager.shared.authorizeCamera { (status) in
             if status == .authorized{
+                
+                self.setDefineValues(cropWidth: IBScreenWidth, cropHeight: IBScreenWidth, screenResolutionW: currentResolutionW, screenResolutionH: currentResolutionH)
                 self.setupCamera()
 //                self.setupUI()
             }
@@ -130,8 +140,8 @@ class LTGPhotoCamear: UIViewController {
 //        photoSettings.flashMode = flashMode
         let previewPixelType = photoSettings.availablePreviewPhotoPixelFormatTypes.first!
         let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
-                             kCVPixelBufferWidthKey as String: 750,
-                             kCVPixelBufferHeightKey as String: 750,
+                             kCVPixelBufferWidthKey as String: 720,
+                             kCVPixelBufferHeightKey as String: 720,
                              ]
         photoSettings.previewPhotoFormat = previewFormat
         imageOutput?.capturePhoto(with: photoSettings, delegate: self)
@@ -155,17 +165,37 @@ extension LTGPhotoCamear: AVCapturePhotoCaptureDelegate {
             print(error.localizedDescription)
         } else {
             
-            if let sampleBuffer = photoSampleBuffer, let previewBuffer = previewPhotoSampleBuffer, let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+//            if let sampleBuffer = photoSampleBuffer, let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer){
+//                
+//                let newImage = CropSubImage(imageData: imageData)
+//                
+//                picData = UIImageJPEGRepresentation(newImage, 0)
+//                if TGPhotoPickerConfig.shared.saveImageToPhotoAlbum{
+//                    self.saveImageToPhotoAlbum(newImage)
+//                }
+//                
+//            }
+            
+            if let sampleBuffer = photoSampleBuffer {
                 
-                let newImage = reSizeImage(imageData: imageData)
+                let cropSampleBuffer:CMSampleBuffer?
                 
-                picData = UIImageJPEGRepresentation(newImage, 0)
+                cropSampleBuffer = self.cropSampleBufferByHardware(buffer: sampleBuffer)
                 
-                if TGPhotoPickerConfig.shared.saveImageToPhotoAlbum{
-                    self.saveImageToPhotoAlbum(newImage)
+                if let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: cropSampleBuffer!, previewPhotoSampleBuffer: cropSampleBuffer){
+                    
+                    picData = imageData
+                    
+                    let image = UIImage(data: imageData)
+                    
+                    if TGPhotoPickerConfig.shared.saveImageToPhotoAlbum{
+                        self.saveImageToPhotoAlbum(image!)
+                    }
                 }
+
+            }
                 
-            }else {
+            else {
                 
                 picData = nil
             }
@@ -176,14 +206,14 @@ extension LTGPhotoCamear: AVCapturePhotoCaptureDelegate {
         canUseAlbum { (canUse) in
             if canUse{
                 
-                let sourceImageRef: CGImage = savedImage.cgImage!
+//                let sourceImageRef: CGImage = savedImage.cgImage!
+//                
+//                let flipImageOrientation = 3
+//        
+//                //图片反转
+//                let normalImage = UIImage.init(cgImage: sourceImageRef, scale: 1, orientation: UIImageOrientation(rawValue: flipImageOrientation)!)
                 
-                let flipImageOrientation = 3
-        
-                //图片反转
-                let normalImage = UIImage.init(cgImage: sourceImageRef, scale: 1, orientation: UIImageOrientation(rawValue: flipImageOrientation)!)
-                
-                UIImageWriteToSavedPhotosAlbum(normalImage, self, #selector(self.imageDidFinishSavingWithErrorContextInfo), nil)
+                UIImageWriteToSavedPhotosAlbum(savedImage, self, #selector(self.imageDidFinishSavingWithErrorContextInfo), nil)
             }
         }
     }
@@ -201,6 +231,145 @@ extension LTGPhotoCamear: AVCapturePhotoCaptureDelegate {
         }
     }
     
+    
+    fileprivate func canUseAlbum(returnClosure:@escaping (Bool)-> ()){
+        TGPhotoPickerManager.shared.authorizePhotoLibrary { (status) in
+            returnClosure(status == .authorized)
+        }
+        /*
+         if PHPhotoLibrary.authorizationStatus() != PHAuthorizationStatus.authorized {
+         let alertView = UIAlertView(title: TGPhotoPickerConfig.shared.PhotoLibraryUsage, message: TGPhotoPickerConfig.shared.PhotoLibraryUsageTip, delegate: nil, cancelButtonTitle: TGPhotoPickerConfig.shared.confirmTitle, otherButtonTitles: TGPhotoPickerConfig.shared.cancelTitle)
+         alertView.tag = TGPhotoPickerConfig.shared.alertViewTag
+         alertView.show()
+         return false
+         }else{
+         return true
+         }
+         */
+    }
+}
+
+extension LTGPhotoCamear{
+    
+    func setDefineValues(cropWidth:CGFloat,cropHeight:CGFloat,screenResolutionW:CGFloat,screenResolutionH:CGFloat){
+        self.cropX = Int(currentResolutionW/IBScreenWidth*0)
+        self.cropY = Int(currentResolutionH/IBScreenHeight*(IBScreenHeight-IBScreenWidth)/2)
+    }
+    
+    func CropSubImage(imageData:Data) -> UIImage {
+        
+        let cropX = Int(currentResolutionW/IBScreenWidth*0)
+        let cropY = Int(currentResolutionH/IBScreenHeight*(IBScreenHeight-IBScreenWidth)/2)
+        
+        let image = UIImage(data: imageData)
+        let subCGImage:CGImage = (image?.cgImage)!
+        
+        let cropRect = CGRect.init(x: cropX, y: cropY, width: 720, height: 720)
+        
+        UIGraphicsBeginImageContext(cropRect.size)
+        
+        let content:CGContext = UIGraphicsGetCurrentContext()!
+        
+        content.draw(subCGImage, in: cropRect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+        
+    }
+    
+    
+    func cropSampleBufferByHardware(buffer:CMSampleBuffer) -> CMSampleBuffer {
+        
+        var cropBuffer:CMSampleBuffer? = nil
+        
+        let imageBuffer = CMSampleBufferGetImageBuffer(buffer)
+        let cropRect = CGRect.init(x: cropX, y: cropY, width: 720, height: 720)
+        
+        var status:OSStatus?
+        
+        var pixbuffer:CVPixelBuffer?
+        var videoInfo:CMVideoFormatDescription?
+        
+        if pixbuffer == nil {
+            
+            let photoSettings = AVCapturePhotoSettings()
+            let previewPixelType = photoSettings.availablePreviewPhotoPixelFormatTypes.first!
+            let previewFormat:NSDictionary = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+                                              kCVPixelBufferWidthKey as String: 720,
+                                              kCVPixelBufferHeightKey as String: 720,
+                                              ]
+            
+//            let bufferPtr = UnsafeMutablePointer<CVPixelBuffer?>.allocate(capacity: 1)
+            
+            status = CVPixelBufferCreate(kCFAllocatorDefault, 720, 720, kCVPixelFormatType_420YpCbCr8BiPlanarFullRange, previewFormat, &pixbuffer)
+            
+            if (status != noErr) {
+                print("Crop CVPixelBufferCreate error \(String(describing: status))")
+                
+                self.label_msg.text = "Crop CMSampleBufferCreateForImageBuffer error \(String(describing: status))"
+                
+                return cropBuffer!
+            }
+        }
+        
+        var ciImage:CIImage = CIImage.init(cvImageBuffer: imageBuffer!)
+        ciImage = ciImage.cropping(to: cropRect)
+        ciImage = ciImage.applying(__CGAffineTransformMake(0, 0, 0, 0, CGFloat(-cropX), CGFloat(-cropY)))
+        
+        var ciContext:CIContext! = nil
+        
+        if ciContext == nil {
+            let eaglContent = EAGLContext.init(api: .openGLES3)
+            ciContext = CIContext.init(eaglContext: eaglContent!, options: nil)
+        }
+        
+        ciContext.render(ciImage, to: pixbuffer!)
+        
+        var sampleTime:CMSampleTimingInfo = CMSampleTimingInfo.init(duration: CMSampleBufferGetDuration(buffer), presentationTimeStamp: CMSampleBufferGetPresentationTimeStamp(buffer), decodeTimeStamp: CMSampleBufferGetDecodeTimeStamp(buffer))
+        
+        if videoInfo == nil {
+            
+//            let bufferPtr = UnsafeMutablePointer<CMVideoFormatDescription?>.allocate(capacity: 1)
+            
+            status = CMVideoFormatDescriptionCreateForImageBuffer(kCFAllocatorDefault, pixbuffer!, &videoInfo)
+            
+            if status != 0{
+                print("Crop CMVideoFormatDescriptionCreateForImageBuffer error \(String(describing: status))")
+                
+                self.label_msg.text = "Crop CMSampleBufferCreateForImageBuffer error \(String(describing: status))"
+                
+                return cropBuffer!
+            }
+            
+        }
+        
+        status = CMSampleBufferCreateForImageBuffer(kCFAllocatorDefault, pixbuffer!, true, nil, nil, videoInfo!, &sampleTime, &cropBuffer)
+        
+        
+        if status != 0{
+            print("Crop CMSampleBufferCreateForImageBuffer error \(String(describing: status))")
+            
+            self.label_msg.text = "Crop CMSampleBufferCreateForImageBuffer error \(String(describing: status))"
+            
+           return cropBuffer!
+        }
+        
+        return cropBuffer!
+        
+    }
+    
+    func samplePointer(ptr:UnsafeMutablePointer<CMSampleTimingInfo>){
+        print("UnsafeMutablePointer:\(ptr)")
+        print("pointee:\(ptr.pointee)")
+    }
+    
+    func sampleBufferPointer(ptr:UnsafeMutablePointer<CMSampleBuffer>){
+        print("UnsafeMutablePointer:\(ptr)")
+        print("pointee:\(ptr.pointee)")
+    }
+    
     func reSizeImage(imageData:Data) -> UIImage {
         
         let image = UIImage(data: imageData)
@@ -212,9 +381,9 @@ extension LTGPhotoCamear: AVCapturePhotoCaptureDelegate {
         
         let sourceImageRef: CGImage = image!.cgImage!
         let newCGImage = sourceImageRef.cropping(to: inRext)!
-
+        
 //        let flipImageOrientation = 3
-//        
+//
 //        //图片反转
 //        let normalImage = UIImage.init(cgImage: newCGImage, scale: 1, orientation: UIImageOrientation(rawValue: flipImageOrientation)!)
         
@@ -237,11 +406,10 @@ extension LTGPhotoCamear: AVCapturePhotoCaptureDelegate {
         //按照给定的矩形区域进行剪裁
         let newImageRef:CGImage = sourceImageRef.cropping(to: inRext)!
         
-        
-        //        let newImageRef:CGImage = sourceImageRef.cropping(to: inRext)!
-        //将CGImageRef转换成UIImage
-        //        UIImage *newImage = [UIImage imageWithCGImage:newImageRef];
-        //        let normalImage:UIImage = UIImage.init(cgImage: newImageRef)
+//        let newImageRef:CGImage = sourceImageRef.cropping(to: inRext)!
+//将CGImageRef转换成UIImage
+//        UIImage *newImage = [UIImage imageWithCGImage:newImageRef];
+//        let normalImage:UIImage = UIImage.init(cgImage: newImageRef)
         
         let flipImageOrientatin = 3
         
@@ -250,19 +418,6 @@ extension LTGPhotoCamear: AVCapturePhotoCaptureDelegate {
         return flipImage
     }
     
-    fileprivate func canUseAlbum(returnClosure:@escaping (Bool)-> ()){
-        TGPhotoPickerManager.shared.authorizePhotoLibrary { (status) in
-            returnClosure(status == .authorized)
-        }
-        /*
-         if PHPhotoLibrary.authorizationStatus() != PHAuthorizationStatus.authorized {
-         let alertView = UIAlertView(title: TGPhotoPickerConfig.shared.PhotoLibraryUsage, message: TGPhotoPickerConfig.shared.PhotoLibraryUsageTip, delegate: nil, cancelButtonTitle: TGPhotoPickerConfig.shared.confirmTitle, otherButtonTitles: TGPhotoPickerConfig.shared.cancelTitle)
-         alertView.tag = TGPhotoPickerConfig.shared.alertViewTag
-         alertView.show()
-         return false
-         }else{
-         return true
-         }
-         */
-    }
+    
 }
+
